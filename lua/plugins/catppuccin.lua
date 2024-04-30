0dff7532bdbf18143c57e7c9c9aee726addb4786
Kien Dang <kien.dang@thescore.com>
2024-04-29 20:32:09 -0400

    upload dotfiles

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  prority = 1000,
+  config = function()
+    vim.cmd.colorscheme "catppuccin"
+  end
+}
+
+
---
+++ lua/plugins/indent-lines.lua
@@ -0,0 +1,7 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibi",
+  config = function()
+    require('ibl').setup({})
+  end
+}
